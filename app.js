3b4b82fd2c59c4b3a0909b488e88bab8ec973417
Lewis Nixon <lewcfn@gmail.com>
2017-07-18 22:36:09 +0100

    feat(invoices): Add a POST handler to the invoice route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var invoices = require('./routes/invoices');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/invoices', invoices);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
