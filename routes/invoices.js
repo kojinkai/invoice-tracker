38bac4c52cde8562455dc758720b33c91281e654
Lewis Nixon <lewcfn@gmail.com>
2017-07-19 14:05:44 +0100

    send contextual message after POST to invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Successfully Posted invoice');
 });
 
 module.exports = router;
