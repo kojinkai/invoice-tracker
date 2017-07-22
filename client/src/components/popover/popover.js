c65766569e31b410389096c47915147090577ed4
Lewis Nixon <lewcfn@gmail.com>
2017-07-22 18:18:24 +0100

    remove unused controls from popover

--- client/src/components/popover/popover.js
+++ client/src/components/popover/popover.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import createModifiers      from '../../lib/createModifiers';
-import Button               from '../button/button';
 import './popover.css';
 
 class Popover extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.props.formData;
-  }
 
   render() {
 
@@ -17,13 +12,11 @@ class Popover extends Component {
 
     return (
       <div className={popoverModifiers}>
-        <div className="popover__header">{this.props.title}</div>
+        <div className="popover__header">
+          <button onClick={this.props.onCancel}>x</button>
+        </div>
         <div className="popover__content">
           {this.props.children}
-        </div>
-        <div className="popover__actions">
-          <Button value="Cancel" onClick={this.props.onCancel} />
-          <Button value="Submit" onClick={this.props.onSubmit} />
         </div>
       </div>
     )
