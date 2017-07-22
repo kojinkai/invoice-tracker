ddc6f245e6ff3498b635fb3a67fb6dfc89d1af8b
Lewis Nixon <lewcfn@gmail.com>
2017-07-22 21:59:11 +0100

    add footer to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import InvoiceList from './components/invoicelist/invoicelist';
 import Masthead    from './components/masthead/masthead';
+import Footer    from './components/footer/footer';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Masthead />
         <InvoiceList />
+        <Footer />
       </div>
     );
   }
