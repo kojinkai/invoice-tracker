7e0b73b8637e20ddabff69f54fc91367b2f265d9
Lewis Nixon <lewcfn@gmail.com>
2017-07-24 18:34:04 +0100

    fix issue in reducer

--- client/src/reducers/dropbox.js
+++ client/src/reducers/dropbox.js
@@ -1,10 +1,11 @@
-const dropbox = (state = { isDragging: false }, action) => {
+const defaults = { isDragging: false }
+
+const dropbox = (state = defaults, action) => {
   switch (action.type) {
 
     case 'SET_DRAG_ACTIVE':
     case 'SET_DRAG_INACTIVE':
-      const activeState = Object.assign({}, state);
-      activeState.isDragging = !state.isDragging 
+      const activeState = Object.assign({}, state, { isDragging: !state.isDragging });
       return activeState
 
     default:
