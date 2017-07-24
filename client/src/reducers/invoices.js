eb8fd127784abca6043cd5482e77b8e2804c3032
Lewis Nixon <lewcfn@gmail.com>
2017-07-24 10:59:19 +0100

    save form data to state

--- client/src/reducers/invoices.js
+++ client/src/reducers/invoices.js
@@ -1,30 +1,35 @@
 const find = require('lodash.find');
 
+const insertInvoiceAtIndex = (state, invoice) => {
+  const updatedState = state.concat()
+  updatedState.splice(invoice.id, 1, invoice)
+  return updatedState
+}
+
 const invoices = (state = [], action) => {
   switch (action.type) {
+
     case 'ADD_INVOICE':
       const updatedState = state.concat({
         id: action.id,
         invoiceData: action.invoiceData
       });
-
       return updatedState
 
     case 'SET_ACTIVE_INVOICE':
+
       const invoiceToActivate = find(state, invoice => invoice.id === action.invoice.id)
       invoiceToActivate.isEditing = true
-
-      const newState = state.concat()
-      newState.splice(invoiceToActivate.id, 1, invoiceToActivate)
-      return newState
+      const editingInvoice = insertInvoiceAtIndex(state, invoiceToActivate)
+      return editingInvoice
 
     case 'SAVE_INVOICE_RECIPIENT_DATA':
+
       const invoiceToSave = find(state, invoice => invoice.isEditing)
       invoiceToSave.isEditing = false
-
-      const newerState = state.concat()
-      newerState.splice(invoiceToSave.id, 1, invoiceToSave)      
-      return newerState
+      invoiceToSave.invoiceData.recipientData = action.recipientData
+      const savingInvoice = insertInvoiceAtIndex(state, invoiceToSave)
+      return savingInvoice
 
     default:
       return state
