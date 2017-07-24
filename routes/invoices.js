24bd8f7c37ef020f7938664824b1442a3a25963e
Lewis Nixon <lewcfn@gmail.com>
2017-07-25 00:10:07 +0100

    send data back async

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  res.send(req.body);
+  const data = [];
+  req.on('data', chunk => {
+    data.push(chunk);
+  });
+
+  req.on('end', () => {
+    const requestBody = data.join();
+    const itemsSaved = JSON.parse(requestBody).length;
+    res.send(`${itemsSaved} items saved`);
+  })
 });
 
 module.exports = router;
