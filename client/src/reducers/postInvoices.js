4c854321b30fd00940f62afd624574af40628a65
Lewis Nixon <lewcfn@gmail.com>
2017-07-24 18:37:03 +0100

    install redux-thunk middleware

---
+++ client/src/reducers/postInvoices.js
@@ -0,0 +1,24 @@
+const defaults = {
+  isFetching: false,
+  response: ''
+};
+
+function postInvoices(state = defaults, action) {
+
+  switch (action.type) {
+    case 'POST_INVOICES':
+      return Object.assign({}, state, {
+        isFetching: true
+      })
+    case 'POST_SUCCESS':
+      return Object.assign({}, state, {
+        isFetching: false,
+        response: '',
+        lastUpdated: action.receivedAt
+      })
+    default:
+      return state
+  }
+}
+
+export default postInvoices
